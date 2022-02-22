4eba5688f41c580b4eea431971c2b057ecbb0625
Fernando Trigueiro <trigueirofernando@hotmail.com>
2022-02-22 08:32:25 -0300

    primeiro commit

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/*!
+* Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+    console.log(String.fromCodePoint(9731, 9733, 9842, 0x2F804));
+    console.log("AVISO");
+    console.log("É possível que o uso deste console permita que invasores falsifiquem sua identidade para roubar informações por meio de um ataque chamado 'Self-XSS'.Não insira nem cole códigos que você não conhece.");
+
+
+
+
+});
